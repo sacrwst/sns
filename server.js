1db7553a02cbd490bceca34d3790b3c3926b9b87
sacrwst <sacrwst@gmail.com>
2023-02-21 23:41:16 +0900

    ✨ users, auth, postsのエンドポイントを分ける

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const app = express()
 const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postsRoute = require("./routes/posts")
 const PORT = 3000
 
 // ミドルウェア
 app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postsRoute)
 
 // app.get("/", (req, res) => {
 //   res.send("Hello")
