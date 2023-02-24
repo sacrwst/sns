b103a0f40bb9f656d708d02005d89a1c03560b34
sacrwst <sacrwst@gmail.com>
2023-02-24 21:44:15 +0900

    ✨ ユーザー更新のエンドポイント

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,22 @@
 const router = require("express").Router()
+const User = require("../models/User")
+
+
+// user update
+router.put("/:id", async (req, res) => {
+  if(req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+      })
+      res.status(200).json("updated")
+    } catch(err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(403)
+  }
+})
 
 // router.get("/", (req, res) => {
 //   res.send("user router")
