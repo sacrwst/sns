bc5be3ff05862fa73909dd59a011848baec2193d
sacrwst <sacrwst@gmail.com>
2023-02-24 21:54:21 +0900

    ✨ ユーザー削除のエンドポイントを追加

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,21 @@ router.put("/:id", async (req, res) => {
   }
 })
 
+
+// user delete
+router.delete("/:id", async (req, res) => {
+  if(req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id)
+      res.status(200).json("deleted")
+    } catch(err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(403)
+  }
+})
+
 // router.get("/", (req, res) => {
 //   res.send("user router")
 // })
