f94baf4031e0e40a592e7f8a3439b8d6458fb35f
sacrwst <sacrwst@gmail.com>
2023-02-24 21:55:35 +0900

    🔥 変数削除

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const User = require("../models/User")
 router.put("/:id", async (req, res) => {
   if(req.body.userId === req.params.id || req.body.isAdmin) {
     try {
-      const user = await User.findByIdAndUpdate(req.params.id, {
+      await User.findByIdAndUpdate(req.params.id, {
         $set: req.body
       })
       res.status(200).json("updated")
@@ -18,12 +18,11 @@ router.put("/:id", async (req, res) => {
   }
 })
 
-
 // user delete
 router.delete("/:id", async (req, res) => {
   if(req.body.userId === req.params.id || req.body.isAdmin) {
     try {
-      const user = await User.findByIdAndDelete(req.params.id)
+      await User.findByIdAndDelete(req.params.id)
       res.status(200).json("deleted")
     } catch(err) {
       return res.status(500).json(err)
