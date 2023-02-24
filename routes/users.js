b34c9462d1bd2c2377ed478f4e6c3858d4166bd0
sacrwst <sacrwst@gmail.com>
2023-02-24 21:54:39 +0900

    ✨ エラー文を追加

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.delete("/:id", async (req, res) => {
       return res.status(500).json(err)
     }
   } else {
-    return res.status(403)
+    return res.status(403).json("It is not your account.")
   }
 })
 
