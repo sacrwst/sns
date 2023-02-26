3d7a70f11f07d7cc651ef501bad7844cdd844ed7
sacrwst <sacrwst@gmail.com>
2023-02-26 22:27:32 +0900

    💡 コメント追加

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const Post = require("../models/Post")
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body)
   try {
+    // 保存
     const savedPost = await newPost.save()
     return res.status(200).json(savedPost)
   } catch(err) {
