2b1800e4b42a480c42704d79d6d6b131cfae2db3
sacrwst <sacrwst@gmail.com>
2023-02-26 19:20:46 +0900

    ✨ 投稿の編集

--- routes/posts.js
+++ routes/posts.js
@@ -11,6 +11,22 @@ router.post("/", async (req, res) => {
   }
 })
 
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id) 
+    if(post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body
+      })
+      return res.status(200).json("edited.")
+    } else {
+      return res.status(403).json("It is not your post.")
+    }
+  } catch(err) {
+    return res.status(500).json(err)
+  }
+})
+
 // router.get("/", (req, res) => {
 //   res.send("posts router")
 // })
